f46e7a1361c85681d10410a14303ce714700608e
emanuel2718 <eramirez2718@gmail.com>
2025-06-21 12:41:22 -0400

    add preliminary palette

--- lua/vanta/palette.lua
+++ lua/vanta/palette.lua
@@ -1,5 +1,44 @@
 ---@class VantaPalette
+---@field black string
+---@field slate string
+---@field grey_dark string
+---@field grey_neutral string
+---@field grey_light string
+---@field blue_dark string
+---@field blue_neutral string
+---@field blue_light string
+---@field blue_bright string
+---@field green_dark string
+---@field green_opaque string
+---@field green_neutral string
+---@field green_light string
+---@field green_bright string
+---@field red string
+---@field yellow string
+---@field violet string
+---@field gold string
+
+---@type VantaPalette
 return {
-  bg0 = "#000000",
-  fg0 = "#ffffff"
+  black = "#000000",
+  slate = "#e2e8f0",
+  grey_dark = "#353839",
+  grey_neutral = "#808080",
+  grey_light = "#757477",
+
+  blue_dark = "#0f172a",
+  blue_neutral = "#0000ff",
+  blue_light = "#2563eb",
+  blue_bright = "#22d3ee",
+
+  green_dark = "#005F00",
+  green_opaque = "#005F60", -- "#14b8a6"
+  green_neutral = "#A2AF49",
+  green_light = "#C6CF4C",
+  green_bright = "#92FFB8",
+
+  yellow = "#fde047",
+  violet = "#5F005F",
+  red = "#5F0000",
+  gold = "#5F5F00",
 }
